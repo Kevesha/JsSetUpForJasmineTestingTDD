d1811d7feba72b8a696173bc0cff887501ec1036
Kevesha Pillay <keveshab@sahomeloans.com>
2018-06-05 10:08:10 +0200

    Setting up for future testing to make my life easier :)

---
+++ readMe.txt
@@ -0,0 +1,5 @@
+You need to do the following
+1. npm install
+2. npm install jasmine
+3. npm install karma
+4. npm install -g jshint
---
+++ tests/nameTheTest.spec.js
@@ -0,0 +1,13 @@
+describe("Method name", function () {
+    describe("given", function () {
+        it("response", function () {
+            //Arrange
+           
+            //Act
+            //let actual = 0;
+            //Assert
+            //let expected = 0;
+            //expect(actual).toBe(expected);
+        });
+    });
+});
